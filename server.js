a24c908d29670ee1fcc78ce68702e6f7b38be363
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-19 09:32:18 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import geminiRouter from "./routes/gemini.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+    origin: "https://swiftmeta.vercel.app",   // your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,  // if you send cookies / tokens
+  })
+););
+app.use(express.json());
+
+app.use("/api/gemini", geminiRouter);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
