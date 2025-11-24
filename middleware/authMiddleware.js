d6610685066b692d6fb0bf9a1223c1221015bea3
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-24 15:12:10 +0200

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export async function requireAuth(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(payload.sub);
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
