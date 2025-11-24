14a6d098c5c716dd49944b9178f217748981f8b4
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-24 15:39:57 +0200

    Create Users.js

---
+++ routes/Users.js
@@ -0,0 +1,15 @@
+import express from "express";
+import User from "../models/User.js";
+import { requireAuth } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.put("/me", requireAuth, async (req, res) => {
+  const { username, avatarUrl } = req.body;
+  if (username) req.user.username = username;
+  if (avatarUrl) req.user.avatarUrl = avatarUrl;
+  await req.user.save();
+  res.json(req.user);
+});
+
+export default router;
+ 
