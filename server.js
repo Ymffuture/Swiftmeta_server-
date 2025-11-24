5dc6d976945291830bb66643eab071f9c13470f9
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-24 17:46:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import geminiRouter from "./routes/gemini.js";
-const authRoutes = require('./routes/auth');
-const postRoutes = require('./routes/posts');
 
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
-dotenv.config();
 
 const app = express();
 app.use(cors({
