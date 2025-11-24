d961092ee61ceaf103f3047e34c528b700166fb6
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-24 17:22:09 +0200

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'No token' });
+  try {
+    const decoded = jwt.verify(token, 'secretkey');
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
