44f3e72e3b688f39794f856eb5c0cacc002002b2
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-25 13:44:48 +0200

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
