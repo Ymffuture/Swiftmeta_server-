5ca0919dea5bd7b7bf3178109ba75a40c4c1bcaf
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-29 16:38:50 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     type: "OAuth2",
     user: process.env.EMAIL_USER,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.JWT_SECRET,
   },
 });
 
