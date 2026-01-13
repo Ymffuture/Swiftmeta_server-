a987a3b09320ce5420a9a696d84956e1453f5fd0
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-13 06:14:33 +0200

    Update ticket.routes.js

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -22,6 +22,6 @@ router.get("/:id", getTicketById);
 router.post("/:id/reply", replyToTicket);
 
 /* Close */
-router.patch("/:id/close", closeTicket);
+router.put("/:ticketid/close", closeTicket);
 
 export default router;
