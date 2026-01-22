1d12f747488263d5269d311e978d633fd309c316
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-22 10:15:23 +0200

    Create passport.js

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/User.js";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/auth/google/callback",
+    },
+    async (_, __, profile, done) => {
+      let user = await User.findOne({ googleId: profile.id });
+
+      if (!user) {
+        user = await User.create({
+          googleId: profile.id,
+          email: profile.emails?.[0]?.value,
+          name: profile.displayName,
+          avatar: profile.photos?.[0]?.value,
+        });
+      }
+
+      done(null, user);
+    }
+  )
+);
