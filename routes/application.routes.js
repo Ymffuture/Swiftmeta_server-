a01141f99436dfd6ab3d0443753ba6d590e9f310
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-27 18:39:41 +0200

    Update application.routes.js

--- routes/application.routes.js
+++ routes/application.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { submitApplication } from "../controllers/application.controller.js";
-import { upload} from "./uploads.js";
+import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
